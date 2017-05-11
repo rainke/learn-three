2ff3e507d7ceb23999a472cfba13e5fabd699eb1
何彭 <rainke@magicare.me>
2017-05-11 11:32:55 +0800

    a little optmize

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import * as THREE from 'three';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 3, 2 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
+const root = document.getElementById('root');
+root.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 3, 2 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
 function render() {
   requestAnimationFrame( render );
   cube.rotation.x += 0.02;
-  // cube.rotation.y += 0.1;
+  cube.rotation.y += 0.1;
   renderer.render( scene, camera );
 }
 render();
